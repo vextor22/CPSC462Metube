40022d5afa702478133da21ec2461b32306dbf73
Ella S Kokinda <ekokind@webapp.cs.clemson.edu>
2017-04-06 16:14:05 -0400

    Pulling data from account to profile page

--- profile.php
+++ profile.php
@@ -3,10 +3,27 @@
 <?php
 
 session_start();
-
 include_once "function.php";
 ?>
-
+<!-- Possibly here need to make a function that will pull from the accout
+	table that will get the information we need-->
+<p>Welcome <?php echo $_SESSION['username'];?></p>
+<?php 
+	$username = $_SESSION['username'];
+	$query = "SELECT firstName, lastname, age, aboutMe FROM account WHERE username='$username'";
+	$result = mysql_query($query);
+	if (!$result){
+		die("User does not exist".mysql_error());
+	}
+	else {
+		$row = mysql_fetch_row($result);
+		$first = $row[0];
+		echo $first;
+		$last = $row[1];
+		$age = $row[2];
+		$aboutMe = $row[3];
+	}
+?>
 
 </body>
 </html>
