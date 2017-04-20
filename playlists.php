308b8e0b2199948c872efa90f5422ec310ad881c
Matthew R Higgins <mrhiggi@webapp.cs.clemson.edu>
2017-04-20 11:43:11 -0400

    added isset to remove username not set error in user profile

---
+++ playlists.php
@@ -0,0 +1,17 @@
+
+<html>
+<head>
+	<title>Playlists</title>
+	<script type="text/javascript" src"js/jquery-latest.parck.js"> </script>
+
+	<?php
+		session_start();
+		include_once "function.php";
+		include('nav-bar.php');
+?>
+</head>
+
+<body>
+</body>
+
+</html>
